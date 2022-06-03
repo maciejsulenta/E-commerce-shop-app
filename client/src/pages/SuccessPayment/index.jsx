2051ae1653311eb16984278220e10e7b71afbbcf
maciejsulenta <maciej.sulenta@gmail.com>
2022-06-03 19:30:14 +0200

    success-payment : feature: structure, styles, link to homepage

--- client/src/pages/SuccessPayment/index.jsx
+++ client/src/pages/SuccessPayment/index.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
-import {Container} from './SuccessPayment'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { Container } from "../../components/Container";
+import { Link } from "react-router-dom";
+import { SuccessContainer, Image, Title, Desc, Button } from "./SuccessPayment";
 
 const SuccessPayment = () => {
-    const location = useLocation();
+  const location = useLocation();
 
   return (
     <Container>
-siema co tam
+      <SuccessContainer>
+        <Image />
+        <div>
+          <Title>Sukces!</Title>
+          <Desc>Transakcja przebiegła pomyślnie</Desc>
+        </div>
+        <Link to="/">
+          <Button>Wróć do strony glównej</Button>
+        </Link>
+      </SuccessContainer>
     </Container>
-  )
-}
+  );
+};
 
-export default SuccessPayment
+export default SuccessPayment;
